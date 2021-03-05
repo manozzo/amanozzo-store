a1037274a6937647848b2e9314b0ba66223dc1f8
Rodrigo Manozzo <rodrigomanozzo@gmail.com>
2021-03-05 02:02:59 -0300

    create rating and product file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Product from './components/product';
 import data from './data';
 function App() {
   return (
-    <div class="grid-container">
-      <header class="row">
+    <div className="grid-container">
+      <header className="row">
         <div>
-          <a class="brand" href="/">AMANOZZO </a>
+          <a className="brand" href="/">AMANOZZO </a>
         </div>
         <div>
           <a href="/cart">Cart</a>
@@ -13,33 +14,16 @@ function App() {
         </div>
       </header>
       <main>
-        <div class="row center">
+        <div className="row center">
           {
             data.products.map(product => (
-              <div key={product._id} class="card">
-                <a href={`/product/${product._id}`}>
-                  <img class="medium" src={product.image} alt={product.name} />
-                </a>
-                <div class="card-body">
-                  <a href={`/product/${product._id}`}>
-                    <h2>{product.name}</h2>
-                  </a>
-                  <div class="rating">
-                    <span><i class="fa fa-star"></i></span>
-                    <span><i class="fa fa-star"></i></span>
-                    <span><i class="fa fa-star"></i></span>
-                    <span><i class="fa fa-star"></i></span>
-                    <span><i class="fa fa-star"></i></span>
-                  </div>
-                  <div class="price">${product.price}</div>
-                </div>
-              </div>
+              <Product key={product._id} product={product}></Product>
             ))
           }
 
         </div>
       </main>
-      <footer class="row center">All right reserved</footer>
+      <footer className="row center">All right reserved</footer>
     </div>
   );
 }
