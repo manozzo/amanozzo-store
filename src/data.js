08b5f03d564ac91bf4bfcecd2959469798ee4475
Rodrigo Manozzo <rodrigomanozzo@gmail.com>
2021-03-05 01:18:46 -0300

    share code on github

---
+++ src/data.js
@@ -0,0 +1,49 @@
+const data = {
+    products: [
+        {
+            _id: '1',
+            name: 'LIPTINT',
+            category: 'LIPTINT',
+            image: '/imgs/product-1.jpeg',
+            price: 50,
+            brand: 'amanozzo',
+            rating: 4.5,
+            numReviews: 50,
+            description: 'hugh quality product',
+        },
+        {
+            _id: '2',
+            name: 'Água Micelar',
+            category: 'Água',
+            image: '/imgs/product-3.jpeg',
+            price: 120,
+            brand: 'amanozzo',
+            rating: 4.5,
+            numReviews: 45,
+            description: 'hugh quality product',
+        },       
+        {
+            _id: '3',
+            name: 'Booster',
+            category: 'Categoria Booster',
+            image: '/imgs/product-2.jpeg',
+            price: 95,
+            brand: 'amanozzo',
+            rating: 4.5,
+            numReviews: 25,
+            description: 'hugh quality product',
+        },       
+        {
+            _id: '4',
+            name: 'Cellroll-ON',
+            category: 'ROLL-ON',
+            image: '/imgs/product-4.jpeg',
+            price: 70,
+            brand: 'amanozzo',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'hugh quality product',
+        }
+    ]
+}
+export default data;
