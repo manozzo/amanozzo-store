f872d3bcca36de1ddcdd85e1ebfbb6a950acebe4
Rodrigo Manozzo <rodrigomanozzo@gmail.com>
2021-03-07 21:20:01 -0300

    homescreen and productscreen creation

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import Product from './components/product';
-import data from './data';
+import { BrowserRouter, Route } from 'react-router-dom';
+import homescreen from './screens/homescreen';
+import productscreen from './screens/productscreen';
+
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">AMANOZZO </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
-      <main>
-        <div className="row center">
-          {
-            data.products.map(product => (
-              <Product key={product._id} product={product}></Product>
-            ))
-          }
-
-        </div>
-      </main>
-      <footer className="row center">All right reserved</footer>
-    </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">AMANOZZO </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
+        <main>
+          <Route path="/product/:id" component={productscreen}></Route>
+          <Route path="/" component={homescreen} exact></Route>
+        </main>
+        <footer className="row center">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
